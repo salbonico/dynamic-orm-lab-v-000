885a5e598d1e35d5e5087aa178427c34ff4f819a
Steve Albonico <salbonico@gmail.com>
2019-01-30 21:01:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,34 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
-def self.table_name
-self.to_s.downcase.pluralize
+
+self.column_names.each do |name|
+  attr_accessor name.to_sym
 end
-
-def self.column_names
-DB[:conn].results_as_hash = true
-sql = "PRAGMA table_info('#{table_name}');"
-
-table_info = DB[:conn].execute(sql)
-column_names = []
-
-table_info.each do |column|
-  column_names << column["name"]
-end
-column_names.compact
-end
-
-
-
-
-
-def initialize(attributes = {})
-  attributes.each do |property, value|
-   self.send("#{property}=",value)
-  end
-
-
-end
-
-
 end
