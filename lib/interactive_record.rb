f800d3801b501eb700a2acab32f0314ea3be0d36
Steve Albonico <salbonico@gmail.com>
2019-01-30 21:24:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ end
 def values_for_insert
 values =[]
  self.class.column_names.each do |col|
-   values << "'send(col)'" unless send(col).nil?
+   values << "'#{send(col)}'" unless send(col).nil?
 end
 values.join(", ")
 end
